919bb46126097ec96496348719495d061531529e
Wasihun Melkamu <melkamuwasihun45@gmail.com>
2025-12-12 00:00:16 -0900

    pass the retun  attemp  as an argument

--- number_gauss_game.py
+++ number_gauss_game.py
@@ -1,32 +1,36 @@
-# from random import randint
+from random import randint
 
-# print('Welcome to the Number Guessing Game!')
-# print("I'm thinking  of a number  between 1 and 100")
-# level=input("choose a difficulty.Type 'easy' or 'hard'" )
-# hard_attempt=10
-# easy_attempt=5
-# def levels(level):
-#     if level=='easy': 
-#         return hard_attempt
-#     else:  
-#         return easy_attempt
-    
-# attempt=levels(level)
+print('Welcome to the Number Guessing Game!')
+print("I'm thinking  of a number  between 1 and 100")
 
-# answer=randint(1,100)
-# def play():
+hard_attempt=10
+easy_attempt=5
+def levels():
+    level=input("choose a difficulty.Type 'easy' or 'hard'" )
+    if level=='easy': 
+        return hard_attempt
+    else:  
+        return easy_attempt
     
-#     global attempt
-#     while attempt>0:
-#         attempt-=1
-#         guess=int(input('Make a guess '))
-#         if guess==answer:
-#             print(f'you win .{attempt} attempts')
-#         elif guess>answer:
-#             print(f'Too high .{attempt} attempts')
-#         elif guess<answer:
-#             print(f'Too Low .{attempt} attempts')
-# play()
-# play_again=input("Do You went to  play again ? Type 'Y' or 'N' ")
-# while play_again=='y':
-#     play()
+
+
+answer=randint(1,100)
+print(answer)
+attempt=levels()
+def play(attempt):
+ 
+    while attempt>0:
+        attempt-=1
+        guess=int(input('Make a guess '))
+        if guess==answer:
+            print(f'you win .{attempt} attempts')
+            attempt=0
+        elif guess>answer:
+            print(f'Too high .{attempt} attempts')
+        elif guess<answer:
+            print(f'Too Low .{attempt} attempts')
+play(attempt)
+
+while input("Do You went to  play again ? Type 'Y' or 'N' ") =='y':
+   
+    play(attempt)
